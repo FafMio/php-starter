e2c5bb805a5f4691c840fcf31fb2807c0bbf1235
Sylvain Roux <fafmio@outlook.fr>
2022-12-02 15:04:52 +0100

    Remove old things

--- app/View_old/global/footer.soft.tpl.php
+++
@@ -1,5 +0,0 @@
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
-<script src="/js/generate.js"></script>
-<script src="/js/copy.js"></script>
-<script src="/js/cover.js"></script>
--- app/View_old/global/head.tpl.php
+++
@@ -1,8 +0,0 @@
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://bootswatch.com/5/lux/bootstrap.min.css">
-
-<script src="https://kit.fontawesome.com/76ac375267.js" crossorigin="anonymous"></script>
